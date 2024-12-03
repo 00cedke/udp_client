b73e26719c16702e0a1476aab040e27d9fb2018d
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-12-03 22:22:53 +0100

    Update udp_client.py

--- udp_client.py
+++ udp_client.py
@@ -1,1 +1,4 @@
-
+class UDPClient:
+  async def handle_client(data, addr, server_socket):
+      print(f"Message received from {addr}: {data.decode()}")
+      server_socket.sendto("Message received".encode(), addr)
